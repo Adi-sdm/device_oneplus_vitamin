8fbf695ae94c65a2ee73716b3aea7ea567c6467b
Mashopy <eliasgheeraert@gmail.com>
2024-11-16 19:18:35 +0100

    vitamin: Switch to python extract-utils
    ImsService.patch have been splitted in two due to how the new extract-utils handle AndroidManifest.xml patching
    media_profiles_V1_0.xml have been moved to device tree due to the new extract-utils impl

--- device.mk
+++ device.mk
@@ -215,7 +215,8 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml
+    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # Net
 PRODUCT_COPY_FILES += \
