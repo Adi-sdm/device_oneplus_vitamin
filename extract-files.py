94cd1e5302e92eb72cdd5b513eb9a0b80934a4d2
Giovanni Ricca <giovanniricca@proton.me>
2025-01-06 21:45:33 +0100

    vitamin: `libshim_sink.so` -> `libaudioclient_shim.so`
    Change-Id: I27c7cea3f2b7feda9736b9f1b1aa52db9202736e

--- extract-files.py
+++ extract-files.py
@@ -48,7 +48,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system_ext/lib64/libsink-mtk.so': blob_fixup()
-        .add_needed('libshim_sink.so'),
+        .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
