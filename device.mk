9d2bdd5b94ea5bc17ff0237b6293fabdbbd46a3a
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-28 03:20:14 +0200

    vitamin: Switch to richtap vibrator service
    Change-Id: If0dd051ed582a19bb7b6ad7314ef67f2ab05a2f1

--- device.mk
+++ device.mk
@@ -357,10 +357,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Vibrator
-$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
-
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    android.hardware.vibrator.service.oplus-richtap
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
